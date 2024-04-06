6841155cdae3c3e3a55190e731fe5185d7a5a3f9
Lucmeister55 <lucavisser20@gmail.com>
2024-04-06 21:17:25 +0200

    update

--- gg.sh
+++
@@ -1,4 +0,0 @@
-awk '{for(i=1; i<=NF; i++) if ($i == 0) {print; exit}}' /data/lvisser/coverage/all_samples.depth.txt
-
-awk 'BEGIN{FS="\t"; OFS="\t"} NR>1 {print $2, $3, $4, $5}'  /data/lvisser/cgi_islands_ucsc.bed\
-  | bedtools sort -i - >  cpg_islands_ucsc_cleaned.bed
